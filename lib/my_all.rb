d9d8f9f06ccb6cd43bdaac586ef3d1724d6e2cf3
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-17 17:17:26 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,11 @@
-\def my_all?(collection)
+def my_all?(collection)
   i = 0 
   block_return_values = []
-  while i < colletion.size 
+  while i < collection.size 
   block_return_values << yield(collection[i])
   i+= 1 
-  
+end
+
   if block_return_values.include?(false)
     false
   else
