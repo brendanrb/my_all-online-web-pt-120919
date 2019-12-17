5d8bc698ee1cfd609ca6331ae8ddcb46269d8a8d
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-17 17:17:16 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,7 +1,7 @@
 def my_all?(collection)
   i = 0 
   block_return_values = []
-  while i < colletion.size 
+  while i < collection.size 
   block_return_values << yield(collection[i])
   i+= 1 
 end
